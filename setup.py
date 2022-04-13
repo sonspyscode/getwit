10bbf6b455da600e157a939c8c1063ac6b20eee0
sonspyscode <79440527+sonspyscode@users.noreply.github.com>
2022-04-13 13:32:06 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+   name='getwit',
+   packages=find_packages(),
+   version='0.1',
+   description='Library yang terhubung dengan Twitter API v2',
+   author='Ahmad Afiq Fitrah, Niken Dwi Wahyu Cahyani, Erwid Mustofa Jadied',
+   install_requires=['requests_oauthlib', 'requests', 'json'],
+)
