20fc4ba96f65df8a506586877e7bb60a1c4d43f6
sonspyscode <79440527+sonspyscode@users.noreply.github.com>
2022-09-03 10:39:54 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
    name='getwit',
    packages=find_packages(),
    version='0.1',
-   description='Library yang terhubung dengan Twitter API v2',
+   description='Getwit as an API-based Acquisition Tool for Twitter',
    author='Ahmad Afiq Fitrah, Niken Dwi Wahyu Cahyani, Erwid Mustofa Jadied',
    install_requires=['requests_oauthlib', 'requests'],
 )
